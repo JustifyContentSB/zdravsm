b25f26e1d79b00652ae63adf71eea26ecbe05e01
Rxpro <wxweb@mail.ru>
2021-05-06 18:44:31 +0300

    Разметка и стилизация страницы на мобилке

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let browserSync = require('browser-sync');
+let concat = require('gulp-concat');
+let rename = require('gulp-rename');
+let autoprefixer = require('gulp-autoprefixer');
+let uglify = require('gulp-uglify-es').default;
+
+gulp.task('html', function() {
+    return gulp.src('app/*.html')
+    .pipe(browserSync.reload({stream: true}))
+})
+
+gulp.task('script', function() {
+    return gulp.src('app/js/*.js')
+    .pipe(browserSync.reload({stream: true}))
+})
+
+gulp.task('scss', function() {
+    return gulp.src('app/scss/index/style.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css/index'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "app",
+            index: 'index.html'
+        }
+    });
+});
+
+// INDEX
+// gulp.task('js', function() {
+//     return gulp.src(['node_modules/slick-carousel/slick/slick.js', 
+//         'node_modules/magnific-popup/dist/jquery.magnific-popup.js'])
+//     .pipe(concat('libs.min.js'))
+//     .pipe(terser())
+//     .pipe(gulp.dest('app/js'))
+//     .pipe(browserSync.reload({stream: true}))
+// });
+
+gulp.task('watch', function() {
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))
+    gulp.watch('app/**/*.html', gulp.parallel('html'))
+    gulp.watch('app/js/*.js', gulp.parallel('script'))
+});
+
+gulp.task('default', gulp.parallel('scss', 'browserSync', 'watch'));
